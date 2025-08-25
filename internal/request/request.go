37af907b02652e8fa99b6bbb38b39e43a09f7546
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-08-25 18:46:40 -0400

    Refactor request parser to include headers state

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,18 +1,20 @@
 package request
 
 import (
+	"GO-HTTPSVR/internal/headers"
+	"bytes"
+	"errors"
 	"fmt"
 	"io"
-	"errors"
-	"bytes"
 )
 
 
 type parserState string
 const (
-	StateInit  parserState = "init"
-	StateDone  parserState = "done"
-	StateError parserState = "error"
+	StateInit     parserState = "init"
+	StateHeaders  parserState = "headers"
+	StateDone     parserState = "done"
+	StateError    parserState = "error"
 )
 
 type RequestLine struct {
@@ -23,6 +25,7 @@ type RequestLine struct {
 
 type Request struct {
 	RequestLine RequestLine
+	Headers	 	*headers.Headers
 	state       parserState
 }
 
@@ -34,6 +37,7 @@ var SEPARATOR = []byte("\r\n")
 func newRequest() *Request {
 	return &Request {
 		state: StateInit,
+		Headers: headers.NewHeaders(),
 	}
 }
 
@@ -69,12 +73,14 @@ func (r *Request) parse(data []byte) (int, error) {
 	read := 0
 	outer:
 		for  {
+			currentData := data[read:]
+
 			switch r.state {
 				case StateError:
 					return 0, ErrorRequestInErrorState
 
 				case StateInit:
-					rl, n, err := parseRequestLine(data[read:])
+					rl, n, err := parseRequestLine(currentData)
 					if err != nil {
 						r.state = StateError
 						return 0, err
@@ -86,14 +92,33 @@ func (r *Request) parse(data []byte) (int, error) {
 
 					r.RequestLine = *rl
 					read += n
-					
-					r.state = StateDone
+					r.state = StateHeaders
+
+				case StateHeaders:
+					n, done, err := r.Headers.Parse(currentData)
+					if err != nil {
+						//r.state = StateError
+						return 0, err
+					}
+
+					if n == 0 {
+						break outer
+					}
+
+					read += n
+
+					if done {
+						r.state = StateDone
+					}
 
 				case StateDone:
 					break outer
+				
+				default:
+					panic(fmt.Sprintf("DEFAULT: We are in an unexpected state: %s\n", r.state))
 			}
 		}
-		return read, nil
+	return read, nil
 }
 
 func (r *Request) done() bool {
