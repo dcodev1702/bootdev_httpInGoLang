37f1a5048338b6a0e280c00f576e217e873fe13d
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-08-25 18:06:41 -0400

    Enhance request tests for header parsing
    Added tests for parsing headers and handling malformed headers.

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,7 +1,6 @@
 package request
 
 import (
-   "strings"
    "testing"
    "io"
 
@@ -55,9 +54,27 @@ func TestRequestLineParse(t *testing.T) {
    assert.Equal(t, "GET", r.RequestLine.Method)
    assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
    assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
-
-   // Test: Invalid number of parts in request line
-   _, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
-   require.Error(t, err)
 }
 
+func TestParseHeaders(t *testing.T) {
+   reader := &chunkReader{
+      data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+      numBytesPerRead: 3,
+   }
+   r, err := RequestFromReader(reader)
+   require.NoError(t, err)
+   require.NotNil(t, r)
+
+   // Assert parsed headers
+   assert.Equal(t, "localhost:42069", r.Headers.Get("host"))
+   assert.Equal(t, "curl/7.81.0", r.Headers.Get("user-agent"))
+   assert.Equal(t, "*/*", r.Headers.Get("accept"))
+
+   // Test: Malformed Header
+   reader = &chunkReader{
+      data:            "GET / HTTP/1.1\r\nHost localhost:42069\r\n\r\n",
+      numBytesPerRead: 3,
+   }
+   r, err = RequestFromReader(reader)
+   require.Error(t, err)
+}
