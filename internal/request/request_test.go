dd4cd09235cf05e45adbcf7b88068ccadd43d9fd
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-08-25 18:46:21 -0400

    Update request_test.go

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -57,6 +57,7 @@ func TestRequestLineParse(t *testing.T) {
 }
 
 func TestParseHeaders(t *testing.T) {
+   // Test: Standard Headers
    reader := &chunkReader{
       data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
       numBytesPerRead: 3,
@@ -64,8 +65,6 @@ func TestParseHeaders(t *testing.T) {
    r, err := RequestFromReader(reader)
    require.NoError(t, err)
    require.NotNil(t, r)
-
-   // Assert parsed headers
    assert.Equal(t, "localhost:42069", r.Headers.Get("host"))
    assert.Equal(t, "curl/7.81.0", r.Headers.Get("user-agent"))
    assert.Equal(t, "*/*", r.Headers.Get("accept"))
