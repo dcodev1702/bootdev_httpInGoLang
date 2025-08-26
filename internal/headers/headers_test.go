8f3216b64b1bfe45cac2099e862bc16ec83062f8
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-08-25 21:48:45 -0400

    Update header tests for case sensitivity

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -16,9 +16,17 @@ func TestHeaderParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers.Get("Host"))
-	assert.Equal(t, "barbar", headers.Get("FooFoo"))
-	assert.Equal(t, "", headers.Get("MissingKey"))
+	host, ok := headers.Get("HOST")
+	assert.True(t, ok)
+	assert.Equal(t, "localhost:42069", host)
+
+	foofoo, ok := headers.Get("FOOFOO")
+	assert.True(t, ok)
+	assert.Equal(t, "barbar", foofoo)
+
+	missingKey, ok := headers.Get("MISSINGKEY")
+	assert.False(t, ok)
+	assert.Equal(t, "", missingKey)
 	assert.Equal(t, 52, n)
 	assert.True(t, done)
 
@@ -38,7 +46,10 @@ func TestHeaderParse(t *testing.T) {
 	n, done, err = headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069,localhost:1702", headers.Get("HOST"))
+
+	host, ok = headers.Get("HOST")
+	assert.True(t, ok)
+	assert.Equal(t, "localhost:42069,localhost:1702", host)
 	assert.False(t, done)
 
 }
