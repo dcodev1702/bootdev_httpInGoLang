5776a8beaa7814242216dc37d3b20764f33bbff2
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-08-25 21:47:59 -0400

    Update request_test.go

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -32,7 +32,7 @@ func TestRequestLineParse(t *testing.T) {
 	
    // Test: Good GET Request line
    reader := &chunkReader{
-      data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+      data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n ",
       numBytesPerRead: 3,
    }
    r, err := RequestFromReader(reader)
@@ -45,7 +45,7 @@ func TestRequestLineParse(t *testing.T) {
 
    // Test: Good GET Request line with path
    reader = &chunkReader{
-      data:            "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+      data:            "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n ",
       numBytesPerRead: 1,
    }
    r, err = RequestFromReader(reader)
@@ -56,22 +56,31 @@ func TestRequestLineParse(t *testing.T) {
    assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
 }
 
-func TestParseHeaders(t *testing.T) {
-   // Test: Standard Headers
+
+
+func TestParseBody(t *testing.T) {
+
+   // Test: Standard Body
    reader := &chunkReader{
-      data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+      data: "POST /submit HTTP/1.1\r\n" +
+         "Host: localhost:42069\r\n" +
+         "Content-Length: 13\r\n" +
+         "\r\n" +
+         "hello world!\n",
       numBytesPerRead: 3,
    }
    r, err := RequestFromReader(reader)
    require.NoError(t, err)
    require.NotNil(t, r)
-   assert.Equal(t, "localhost:42069", r.Headers.Get("host"))
-   assert.Equal(t, "curl/7.81.0", r.Headers.Get("user-agent"))
-   assert.Equal(t, "*/*", r.Headers.Get("accept"))
+   assert.Equal(t, "hello world!\n", string(r.Body))
 
-   // Test: Malformed Header
+   // Test: Body shorter than reported content length
    reader = &chunkReader{
-      data:            "GET / HTTP/1.1\r\nHost localhost:42069\r\n\r\n",
+      data: "POST /submit HTTP/1.1\r\n" +
+         "Host: localhost:42069\r\n" +
+         "Content-Length: 20\r\n" +
+         "\r\n" +
+         "partial content",
       numBytesPerRead: 3,
    }
    r, err = RequestFromReader(reader)
